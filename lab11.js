fec70a6d7190040823489ea4dc83915be1bd0896
Laurie Sidwell <laurie.sidwell@gm.com>
2017-07-29 13:41:35 -0400

    Updated

--- lab11.js
+++ lab11.js
@@ -3,15 +3,15 @@ app.config(function($routeProvider, $locationProvider) {
         $routeProvider
         .when('/',
         {controller: 'SimpleController',
-        templateUrl: 'view1.html'
+        templateUrl: '#/view1.html'
         })
         .when('/cat',
         {controller: 'SimpleController',
-        templateUrl: 'view2.html'
+        templateUrl: '#/view2.html'
         })
         .when('/dog',
         {controller: 'SimpleController',
-        templateUrl: 'view2.html'
+        templateUrl: '#/view2.html'
         })
         .otherwise({ redirectTo: '/' });
         $locationProvider.hashPrefix('');
