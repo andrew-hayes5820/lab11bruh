56bbf059bf35cd8c85c3bf8cba866efd07c089c3
Andrew Hayes <andy.hayes13@gmail.com>
2017-07-29 11:24:39 -0400

    all the things2!

--- lab11.js
+++ lab11.js
@@ -1,16 +1,18 @@
 var app = angular.module('gratefulDead', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
-$routeProvider
-.when('/',
-{
-controller: 'SimpleController',
-templateUrl: 'partials/view1.html'
-})
-.when('/view2',
-{
-controller: 'SimpleController',
-templateUrl: 'partials/view2.html'
-})
-.otherwise({ redirectTo: '/' });
-$locationProvider.hashPrefix('');
+        $routeProvider
+        .when('/',
+        {controller: 'SimpleController',
+        templateUrl: 'partials/view1.html'
+        })
+        .when('/cat',
+        {controller: 'SimpleController',
+        templateUrl: 'partials/view2.html'
+        })
+        .when('/dog',
+        {controller: 'SimpleController',
+        templateUrl: 'partials/view2.html'
+        })
+        .otherwise({ redirectTo: '/' });
+        $locationProvider.hashPrefix('');
 });
